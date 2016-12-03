db63716e33ce6ce9dcd31a81a609af608acb7f27
Koichi Yamada <koichi.yamada@jrits.ricoh.co.jp>
2016-12-04 05:22:52 +0900

    ch04-ex01 new

---
+++ src/ch04/ex01/TextInput.java
@@ -0,0 +1,37 @@
+package ch04.ex01;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+/**
+ * テキスト入力をラベルに表示する
+ *
+ * @author 山田晃一
+ */
+public class TextInput extends Application {
+
+	/**
+	 * 下位互換エントリポイント
+	 *
+	 * @param args
+	 *            引数
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		final Label message = new Label("Hello, FX");
+		message.setFont(new Font(100));
+		final TextField input = new TextField("Hello, FX");
+		input.textProperty().addListener((p, o, n) -> message.setText(n));
+		primaryStage.setScene(new Scene(new VBox(message, input)));
+		primaryStage.show();
+	}
+}
