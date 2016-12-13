891e6f4c4f10c742f7fe854b602aa0b1c1e1b23f
Koichi Yamada <koichi.yamada@jrits.ricoh.co.jp>
2016-12-14 03:27:08 +0900

    ch03 ex22 new

---
+++ src/ch03/ex22/CompletableFutureFlatMap.java
@@ -0,0 +1,10 @@
+package ch03.ex22;
+
+/**
+ * <pre>
+ * これです。
+ * <U> CompletableFuture<U>	thenCompose(Function<? super T,? extends CompletionStage<U>> fn)
+ * </pre>
+ */
+public class CompletableFutureFlatMap {
+}
